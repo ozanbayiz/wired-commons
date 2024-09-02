cad42bf75ecfd41e746264440f7572eddf0fe519
Ozan Bayiz <ozanbayiz@gmail.com>
2024-09-02 15:55:38 -0700

    i think i'm getting the hang of this

---
+++ main.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import folium
+import os
+from streamlit_folium import st_folium, folium_static
+from ckanapi import RemoteCKAN
+from dotenv import load_dotenv
+
+from utils import reset_map, update_map
+
+load_dotenv('.env')
+
+if 'ckan' not in st.session_state:
+    st.session_state.ckan = RemoteCKAN('https://wifire-data.sdsc.edu/', apikey=os.environ['apiKey'])
+
+st.markdown(
+    """
+    <style>
+            .block-container {
+                padding-top: 1rem;
+                padding-bottom: 0rem;
+                padding-left: 0rem;
+                padding-right: 0rem;
+                margin-bottom: 0rem;
+                margin-left: 0rem;
+                margin-right: 0rem;
+            }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 500px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title('WIRED Map Interface')
+
+# The Map
+map_placeholder = st.empty()
+map_updated = False
+if 'map' not in st.session_state:  
+    # create and display new map
+    m = reset_map()
+    folium.plugins.Fullscreen(
+            position='topright',
+            title='Full Screen',
+            title_cancel='Exit Full Screen',
+            force_separate_button=True
+        ).add_to(m)
+    st.session_state.map = m
+    with map_placeholder:
+        folium_static(st.session_state.map)
+    # also create cache to store existing layers for quicker loading
+    # # Maybe can look into compressing somehow?
+    st.session_state.cached_layers = {}
+    st.session_state.active_layers = {}
+else:
+    with map_placeholder:
+        folium_static(st.session_state.map)
+
+error_placeholder = st.empty()
+
+# Sidebar
+with st.sidebar:
+    st.write('information about data')
+    search = st.text_input('Search WIFIRE Data Catalog')
+
+    if (len(search) > 0 and 'search' not in st.session_state) \
+            or ('search' in st.session_state and st.session_state.search != search):
+        st.session_state.search = search
+        params = {
+            'q':f'title:"{search}" AND (res_format:"Esri REST" OR res_format:"ArcGIS GeoServices REST API" OR res_format:GeoJSON OR res_format:GeoTIFF OR res_format:TIFF)',
+            'start': 0,
+            'rows': 100
+        }
+        with st.spinner('searching WIFIRE Data Catalog'):
+            response = st.session_state.ckan.action.package_search(**params)
+            st.session_state.search_results = response['results']
+
+    # Checkboxes
+    if 'search_results' in st.session_state:
+        with st.expander("Search Results: ", expanded=True):
+            result_list = {}
+            for result in st.session_state.search_results:
+                col1, col2 = st.columns([2,1])
+                with col1:
+                    result_list[result['id']] = st.checkbox(
+                        result['title'], 
+                        key=result['id'], 
+                        value=(result['id'] in st.session_state.active_layers),
+                        on_change=update_map,
+                        args=(result, map_placeholder, error_placeholder)
+                    )
+                with col2:
+                    st.link_button("View Metadata", url='https://wifire-data.sdsc.edu/dataset/' + result['id']) 
+        st.session_state.result_list = result_list
---
+++ utils.py
@@ -0,0 +1,314 @@
+import warnings
+import requests
+import urllib.request
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, Resampling, transform_bounds
+
+import json
+from io import BytesIO
+
+import folium
+from streamlit_folium import folium_static
+import streamlit as st
+
+import urllib.parse, urllib.request
+import concurrent.futures
+
+# css_colors = [
+#     "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", 
+#     "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", 
+#     "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", 
+#     "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", 
+#     "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", 
+#     "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", 
+#     "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", 
+#     "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", 
+#     "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", 
+#     "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", 
+#     "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", 
+#     "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", 
+#     "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", 
+#     "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", 
+#     "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", 
+#     "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", 
+#     "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", 
+#     "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", 
+#     "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", 
+#     "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", 
+#     "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
+# ]
+
+css_colors = [
+    "black", "silver", "gray", "white", "maroon", "red", "purple", "fuchsia", 
+    "green", "lime", "olive", "yellow", "navy", "blue", "teal", "aqua", 
+    "orange", "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", 
+    "beige", "bisque", "blanchedalmond", "blueviolet", "brown", "burlywood", 
+    "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", 
+    "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", 
+    "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", 
+    "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", 
+    "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", 
+    "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", 
+    "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", 
+    "gainsboro", "ghostwhite", "gold", "goldenrod", "greenyellow", 
+    "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", 
+    "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", 
+    "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", 
+    "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", 
+    "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", 
+    "lightsteelblue", "lightyellow", "limegreen", "linen", "magenta", 
+    "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", 
+    "mediumseagreen", "mediumslateblue", "mediumspringgreen", 
+    "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", 
+    "mistyrose", "moccasin", "navajowhite", "oldlace", "olive", "olivedrab", 
+    "orange", "orangered", "orchid", "palegoldenrod", "palegreen", 
+    "paleturquoise", "palevioletred", "papayawhip", "peachpuff", 
+    "peru", "pink", "plum", "powderblue", "rebeccapurple", "rosybrown", 
+    "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", 
+    "seashell", "sienna", "skyblue", "slateblue", "slategray", "slategrey", 
+    "snow", "springgreen", "steelblue", "tan", "thistle", "tomato", 
+    "turquoise", "violet", "wheat", "whitesmoke", "yellowgreen"
+]
+
+
+def fetch_all_features(base_url):
+    # Get record extract limit
+    url_string = base_url + "?f=json"
+    j = urllib.request.urlopen(url_string)
+    js = json.load(j)
+    max_records_count = int(js["maxRecordCount"])
+    # max_records_count = min(max_records_count, 800)
+    # Get object ids of features
+    fields = "*"
+    where = "1=1"
+    url_string = base_url + "/query?where={}&returnIdsOnly=true&f=json".format(where)
+    j = urllib.request.urlopen(url_string)
+    js = json.load(j)
+    id_field = js["objectIdFieldName"]
+    id_list = js["objectIds"]
+    # this could be optimized maybe
+    id_list.sort()
+    num_of_records = len(id_list)
+    request_number = 0
+    # create URLs for requests
+    url_strings = []
+    for i in range(0, num_of_records, max_records_count):
+        request_number += 1
+        to_rec = i + (max_records_count - 1)
+        if to_rec > num_of_records:
+            to_rec = num_of_records - 1
+        from_id = id_list[i]
+        to_id = id_list[to_rec]
+        where = '{} BETWEEN {} AND {}'.format(id_field, from_id, to_id)
+        url_string = base_url + "/query?where={}&returnGeometry=true&outFields={}&f=geojson".format(where, fields)
+        url_strings.append(url_string)
+    # specify helper function which does the fetching
+    def load_features(urlstring):
+        succeed = False
+        while not succeed:
+            try:
+                with warnings.catch_warnings(action='ignore'):
+                    resp = requests.get(urlstring, verify=False)
+                data = resp.json()
+                gdf = gpd.GeoDataFrame.from_features(data['features'], crs='EPSG:4326')
+                gdf['geometry'] = (
+                    gdf['geometry']
+                    .simplify(tolerance=0.01, preserve_topology=True) # lower resolution
+                    .buffer(0) # fix invalid geometries
+                )
+                succeed = True
+                return gdf
+            except:
+                print ('Failed to load {}'.format(urlstring))
+    # thread pool to fetch features
+    with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+        features_list = list(executor.map(load_features, url_strings))
+    final_gdf = pd.concat(features_list)
+    return final_gdf
+
+def fetch_geojson(url):
+    geojson = requests.get(url).json()
+    return geojson
+
+def fetch_opentopo_geojson(url):
+    information = requests.get(url).json()
+    json_link = [x['href'] for x in information['links'] if x['rel'] == 'child' and 'raster' not in x['href']]
+    if len(json_link) > 0:
+        geojson = requests.get(json_link[0]).json()
+        feature_collection = {
+            "type": "FeatureCollection",
+            "features": [
+                geojson
+            ]
+        }
+        return feature_collection
+
+def create_vector_layer(geojson, dataset_title):
+    if 'errors' in geojson:
+        st.error('Layer could not be fetched', icon="🚨")        
+        return
+    else:
+        layer_color = random.choice(css_colors)
+        style_function=lambda x: {
+            'color':layer_color
+        }
+        layer = folium.GeoJson(
+            geojson,
+            name=dataset_title,
+            style_function=style_function
+        )
+    return layer
+
+
+
+## Create Raster Layers
+################################
+
+raster_cmaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
+
+def fetch_raster_bytes(url):
+    response = requests.get(url)
+    return BytesIO(response.content)
+
+def create_raster_layer(raster_bytes, dataset_name, downsample_data=True):
+    with rio.open(raster_bytes) as src:
+        nodata_value = src.nodata
+        src_crs = src.crs
+        # reproject bounds to EPSG:4326
+        src_bounds = src.bounds
+        minx, miny, maxx, maxy = transform_bounds(src_crs, 'EPSG:4326', *src_bounds)
+        layer_bounds = ((miny, minx), (maxy, maxx))
+
+        # reproject data to EPSG:3857
+        transform, width, height = calculate_default_transform(
+            src_crs, 'EPSG:3857', src.width, src.height, *src.bounds)
+        # reprojected_data = np.empty((src.count, height, width), dtype=src.dtypes[0])
+
+        data = np.empty((height, width), dtype=src.dtypes[0])
+        # reproject
+        reproject(
+            source=rio.band(src, 1),
+            destination=data,
+            src_transform=src.transform,
+            src_crs=src_crs,
+            dst_transform=transform,
+            dst_crs='EPSG:3857',
+            resampling=Resampling.nearest
+        )
+        if downsample_data:
+            data = data[::50, ::50]
+        data[np.where(data==nodata_value)] = np.nan
+        # create layer
+        cmap = plt.get_cmap(random.choice(raster_cmaps))
+        # raster_cmap_idx = (raster_cmap_idx + 1) % len(raster_cmaps)
+        norm = plt.Normalize(vmin=np.nanmin(data), vmax=np.nanmax(data))
+        image_data = cmap(norm(data))
+        alpha_channel = np.where(np.isfinite(data), 255, 0)
+        rgba_image_data = np.dstack((image_data[:, :, :3] * 255, alpha_channel)).astype(np.uint8)
+        raster_layer = folium.raster_layers.ImageOverlay(
+            name=dataset_name,
+            image=rgba_image_data,
+            bounds=layer_bounds,
+            transparent=True,
+            fmt="image/png8",
+            tiled=True,
+            interactive=True,
+            cross_origin=False,
+            zindex=1,
+        )
+    return raster_layer
+
+def get_layer(dataset):
+    dataset_id = dataset['id']
+    # use cached layer if exists
+    if dataset_id in st.session_state.cached_layers.keys():
+        layer = st.session_state.cached_layers[dataset_id]
+    else:
+        layer = None
+        if dataset['owner_org'] == 'e2d487d1-6973-487c-bb20-a11744d9e1ea': #OpenTopography
+            with st.spinner(f'fetching data for {dataset["title"]}'):
+                gdf = fetch_opentopo_geojson(dataset['url'])
+            with st.spinner(f'creating layer for {dataset["title"]}'):
+                layer = create_vector_layer(gdf, dataset['title'])
+        else:
+            for resource in dataset['resources']:
+                if resource['url']:
+                    # for FeatureServices
+                    if 'REST' in resource['format'] \
+                        and resource['url'][-1] in '0123456789': # make sure it's a valid Feature Service
+                        try:
+                            with st.spinner(f'fetching data for {dataset["title"]}'):
+                                gdf = fetch_all_features(resource['url'])
+                            with st.spinner(f'creating layer for {dataset["title"]}'):
+                                layer = create_vector_layer(gdf, dataset['title']) 
+                            break   
+                        except:
+                            st.error('Something went wrong :(')
+                            pass
+                    # for GeoJSONs
+                    elif resource['format'] == 'GeoJSON': 
+                        with st.spinner(f'fetching data for {dataset["title"]}'):
+                            geojson = fetch_geojson(resource['url'])
+                        with st.spinner(f'creating layer for {dataset["title"]}'):
+                            layer = create_vector_layer(geojson, dataset['title'])
+                        break # only add one layer
+                    
+                    #For Raster Files
+                    elif resource['format'] == 'GeoTIFF' or resource['format'] == 'TIFF':
+                        with st.spinner(f'fetching data for {dataset["title"]}'):
+                            raster_bytes = fetch_raster_bytes(resource['url'])
+                        with st.spinner(f'creating layer for {dataset["title"]}'):
+                            layer = create_raster_layer(raster_bytes, dataset['title'])
+                        break # only add one layer
+        # cache new layer
+        if layer:
+            st.session_state.cached_layers[dataset_id] = layer
+    return layer
+
+
+def reset_map():
+    ## what if we preloaded the datasets that we already have? 
+    ## Add them here so the map is initialized with the datasets
+    map = folium.Map(location=[37.1661, -119.44944], zoom_start=4)   
+    return map
+
+def update_map(dataset, map_placeholder, error_placeholder):
+    # update active layers
+    active_layers = st.session_state.active_layers
+    if dataset['id'] in active_layers:
+        del active_layers[dataset['id']]
+    else:
+        active_layers[dataset['id']] = dataset
+    st.session_state.active_layers = active_layers
+
+    m = reset_map()
+    # add all layers to new map
+    error_datasets = []
+    with error_placeholder:
+        for dataset_id in active_layers:
+            layer = get_layer(active_layers[dataset_id])
+            if layer:
+                layer.add_to(m)
+            else:
+                error_datasets.append(dataset['title'])
+                st.error(f'Layer for {dataset['title']} could not be fetched', icon="🚨")
+    folium.plugins.Fullscreen(
+        position='topright',
+        title='Full Screen',
+        title_cancel='Exit Full Screen',
+        force_separate_button=True
+    ).add_to(m)
+    if len(active_layers) > 1:
+        folium.LayerControl().add_to(m)
+    with map_placeholder:
+        folium_static(m)
+    st.session_state.map = m
+    with error_placeholder:
+        for error_dataset in error_datasets:
+            st.error(f'Layer for {error_dataset} could not be fetched', icon="🚨")        
