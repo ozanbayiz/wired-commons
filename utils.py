45a1659f2b152c097596be151679ca850d0c175d
Ozan Bayiz <ozanbayiz@gmail.com>
2024-09-04 16:41:54 -0700

    yes

--- utils.py
+++ utils.py
@@ -20,29 +20,6 @@ import streamlit as st
 import urllib.parse, urllib.request
 import concurrent.futures
 
-# css_colors = [
-#     "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", 
-#     "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", 
-#     "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", 
-#     "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", 
-#     "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", 
-#     "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", 
-#     "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", 
-#     "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", 
-#     "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", 
-#     "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", 
-#     "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", 
-#     "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", 
-#     "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", 
-#     "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", 
-#     "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", 
-#     "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", 
-#     "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", 
-#     "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", 
-#     "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", 
-#     "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", 
-#     "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
-# ]
 
 css_colors = [
     "black", "silver", "gray", "white", "maroon", "red", "purple", "fuchsia", 
@@ -110,22 +87,19 @@ def fetch_all_features(base_url):
         url_strings.append(url_string)
     # specify helper function which does the fetching
     def load_features(urlstring):
-        succeed = False
-        while not succeed:
-            try:
-                with warnings.catch_warnings(action='ignore'):
-                    resp = requests.get(urlstring, verify=False)
-                data = resp.json()
-                gdf = gpd.GeoDataFrame.from_features(data['features'], crs='EPSG:4326')
-                gdf['geometry'] = (
-                    gdf['geometry']
-                    .simplify(tolerance=0.01, preserve_topology=True) # lower resolution
-                    .buffer(0) # fix invalid geometries
-                )
-                succeed = True
-                return gdf
-            except:
-                print ('Failed to load {}'.format(urlstring))
+        try:
+            with warnings.catch_warnings(action='ignore'):
+                resp = requests.get(urlstring, verify=False)
+            data = resp.json()
+            gdf = gpd.GeoDataFrame.from_features(data['features'], crs='EPSG:4326')
+            gdf['geometry'] = (
+                gdf['geometry']
+                .simplify(tolerance=0.01, preserve_topology=True) # lower resolution
+                .buffer(0) # fix invalid geometries
+            )
+            return gdf
+        except:
+            print ('Failed to load {}'.format(urlstring))
     # thread pool to fetch features
     with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
         features_list = list(executor.map(load_features, url_strings))
